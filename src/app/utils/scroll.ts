1490284f55b9320daea6ff8423f2f8766250cb27
Luis González <luismoreno785@gmail.com>
2024-07-26 16:36:57 -0400

    improving performance

--- src/app/utils/scroll.ts
+++ src/app/utils/scroll.ts
@@ -1,5 +1,3 @@
-import { MouseEvent } from "react";
-
 export const scrollTo = (event: MouseEvent, targetId: string, offsetPixels: number = 280) => {
   event.preventDefault();
   const targetElement = document.getElementById(targetId);
