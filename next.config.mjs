3cf6068ebe91cc3be3951e7562e74fb9035df686
Luis González <luismoreno785@gmail.com>
2024-08-24 18:31:10 -0400

    restored next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.m?js/,
-      resolve: {
-        fullySpecified: false,
-      },
-    });
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
